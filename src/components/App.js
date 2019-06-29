6fc05853b6315d55416a9895deaeb845e9bc093f
Earthy <o_nongearth_o@hotmail.com>
2019-06-29 22:25:50 +0700

    re-code stucture
    make a new code structure

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      word = ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    )
+  }
+}
+
+export default App
